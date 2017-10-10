e4773bdf12a565ff17c31500a0815b90fa2faf47
David Dan <davidwdan@gmail.com>
2017-10-10 11:50:06 -0400

    Use static constructor for static event loop

--- src/Client.php
+++ src/Client.php
@@ -3,7 +3,6 @@
 namespace Rx\React;
 
 use function EventLoop\getLoop;
-use function EventLoop\setLoop;
 use React\EventLoop\LoopInterface;
 use React\HttpClient\Client as HttpClient;
 use React\Socket\Connector;
@@ -13,15 +12,15 @@ final class Client
 {
     private $client;
 
-    public function __construct(LoopInterface $loop = null, ConnectorInterface $connector = null)
+    public function __construct(LoopInterface $loop, ConnectorInterface $connector = null)
     {
-        if ($loop) {
-            setLoop($loop);
-        }
-
-        $loop         = getLoop();
         $connector    = $connector ?: new Connector($loop);
         $this->client = new HttpClient($loop, $connector);
+    }
+
+    public static function createWithDefaults(): self
+    {
+        return new self(getLoop());
     }
 
     public function request(string $method, string $url, string $body = null, array $headers = [], string $protocolVersion = '1.1'): HttpObservable
--- src/Http.php
+++ src/Http.php
@@ -14,36 +14,36 @@ final class Http
         $headers         = $request->getHeaders();
         $protocolVersion = $request->getProtocolVersion();
 
-        return (new Client)->request($method, $url, $body, $headers, $protocolVersion);
+        return Client::createWithDefaults()->request($method, $url, $body, $headers, $protocolVersion);
     }
 
     public static function get(string $url, array $headers = [], string $protocolVersion = '1.1'): HttpObservable
     {
-        return (new Client)->request('GET', $url, null, $headers, $protocolVersion);
+        return Client::createWithDefaults()->request('GET', $url, null, $headers, $protocolVersion);
     }
 
     public static function post(string $url, string $body = null, array $headers = [], string $protocolVersion = '1.1'): HttpObservable
     {
-        return (new Client)->request('POST', $url, $body, $headers, $protocolVersion);
+        return Client::createWithDefaults()->request('POST', $url, $body, $headers, $protocolVersion);
     }
 
     public static function put(string $url, string $body = null, array $headers = [], string $protocolVersion = '1.1'): HttpObservable
     {
-        return (new Client)->request('PUT', $url, $body, $headers, $protocolVersion);
+        return Client::createWithDefaults()->request('PUT', $url, $body, $headers, $protocolVersion);
     }
 
     public static function delete(string $url, array $headers = [], string $protocolVersion = '1.1'): HttpObservable
     {
-        return (new Client)->request('DELETE', $url, null, $headers, $protocolVersion);
+        return Client::createWithDefaults()->request('DELETE', $url, null, $headers, $protocolVersion);
     }
 
     public static function patch(string $url, string $body = null, array $headers = [], string $protocolVersion = '1.1'): HttpObservable
     {
-        return (new Client)->request('PATCH', $url, $body, $headers, $protocolVersion);
+        return Client::createWithDefaults()->request('PATCH', $url, $body, $headers, $protocolVersion);
     }
 
     public static function head(string $url, array $headers = [], string $protocolVersion = '1.1'): HttpObservable
     {
-        return (new Client)->request('HEAD', $url, null, $headers, $protocolVersion);
+        return Client::createWithDefaults()->request('HEAD', $url, null, $headers, $protocolVersion);
     }
 }
